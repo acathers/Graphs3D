ea3c0a5f5b96c1ba3b9b4eba1813e08a08702105
Keanu Williams <keanucwilliams@gmail.com>
2020-02-26 17:35:42 -1000

    Minor aesthetic changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
 function refreshGraph() {
+    // remove current graph
     d3.selectAll('svg').remove();
 
+    // allow for domain adjusting
+    let def_width = 0;
+    if ($('#end').dropdown("get value") - $('#start').dropdown("get value") >= 70) {
+        def_width = 2000;
+    }
+    else if ($('#end').dropdown("get value") - $('#start').dropdown("get value") >= 30) {
+        def_width = 1500;
+    }
+    else if ($('#end').dropdown("get value") - $('#start').dropdown("get value") >= 10) {
+        def_width = 1000;
+    }
+    else if ($('#end').dropdown("get value") - $('#start').dropdown("get value") >= 2) {
+        def_width = 500;
+    }
+    else {
+        def_width = 200;
+    }
+
+    // build window
     const margin = {top: 10, right: 30, bottom: 30, left: 40},
-        width = 2000 - margin.left - margin.right,
+        width = def_width - margin.left - margin.right,
         height = 710 - margin.top - margin.bottom;
     const x = d3.scaleBand()
         .range([0, width])
@@ -10,7 +30,7 @@ function refreshGraph() {
     const y = d3.scaleLinear()
         .range([height, 0]);
 
-// append the svg object to the body of the page
+    // append the svg object to the body of the page
     const svg = d3.select("#graph")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -19,7 +39,7 @@ function refreshGraph() {
         .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
 
-// get the data
+    // get the data
     d3.csv("assets/us-population-1990-to-2016.csv", function (d) {
         if (d.year >= $('#start').dropdown("get value") && d.year <= $('#end').dropdown("get value")) {
             return {
@@ -29,7 +49,7 @@ function refreshGraph() {
         }
     }).then(function (data) { //when promise returned
 
-// set the dimensions and margins of the graph
+        // set the dimensions and margins of the graph
         x.domain(data.map(function(d) { return d.year; }));
         y.domain([0, 350]);
 
